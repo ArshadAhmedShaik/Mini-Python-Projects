e2e3026860e6ca3c4a1990ba66740d7abbb6606d
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-31 23:14:59 +0530

    Added two more cool projects!

---
+++ NEWS_API/main.py
@@ -0,0 +1,32 @@
+import requests #pip install requests
+
+query = input("Enter your query: ")
+api="8a9d2d41c6e74698a7428144aeee2322"
+
+url = f"https://newsapi.org/v2/everything?q={query}&from=2025-08-30&sortBy=publishedAt&apiKey={api}"
+
+r = requests.get(url)
+
+data = r.json()
+articles = data["articles"]
+
+with open("news.txt", "w", encoding="utf-8") as f:
+  i = 1
+  f.write(f"Total articles fetched: {data['totalResults']}\n")
+  for index,article in enumerate(articles):
+      source = article["source"]
+      name = source["name"]
+      f.write("-------------------------------------------------\n")
+      f.write(f"Article {index+1}:\n")
+      f.write(f"Name : {name}\n")
+      f.write(f"Author: {article['author']}\n")
+      f.write(f"Title: {article['title']}\n")
+      f.write(f"URL: {article['url']}\n")
+      f.write("\n")
+      f.write(f"{article['content']}\n")
+      f.write("-------------------------------------------------")
+      i += 1
+
+
+
+
---
+++ WEATHER_FORECAST/main.py
@@ -0,0 +1,30 @@
+import requests
+
+api_key = f"6b8fb35152e7465b8ec172246253108"
+url = f"http://api.weatherapi.com/v1/current.json"
+place = input("Enter city: ")
+parameters = {
+      "key": api_key,
+      "q": place
+}
+
+r = requests.get(url, params = parameters)
+data = r.json()
+
+with open("fore_cast.txt", "w") as f:
+     location = data.get("location", "N/A")
+     current = data.get("current", "N/A")
+     f.write(f"Location: {location.get("name", "N/A")}\n")
+     f.write(f"Region: {location.get("region", "N/A")}\n")
+     f.write(f"Country: {location.get("country", "N/A")}\n")
+     f.write(f"-------------------------------------------\n")
+     f.write(f"Temperature in Celcius: {current.get("temp_c", "N/A")}\n")
+     f.write(f"Temperature in Farenheit: {current.get("temp_f", "N/A")}\n")
+     f.write(f"-------------------------------------------\n")
+     f.write(f"Condition:\n")
+     f.write(f"{current.get("condition", "N/A").get("text", "N/A")}")
+
+     
+      
+
+
