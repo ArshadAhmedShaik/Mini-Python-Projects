50800b0b962c22215fcd93b3114c277a6cdf9fa9
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-01 10:22:06 +0530

    Added AI chatbot!

---
+++ AI_CHATBOT/main.py
@@ -0,0 +1,26 @@
+from groq import Groq
+
+key = "------API---KEY------"
+
+client = Groq(api_key=key)
+
+# need to store conversational history:
+
+messages_list = [{"role": "system", "content":"You are a helpful assistant"}]
+
+print("Type 'exit' to quit.\n")
+
+while True:
+    message = input("You: ")
+    if message.lower() in ["exit", "quit"]:
+        break
+    
+    messages_list.append({"role": "user", "content": message})
+
+    chat_completion = client.chat.completions.create(
+        messages = messages_list,
+        model="openai/gpt-oss-20b",
+        stream=False,
+    )
+
+    print("Bot:", chat_completion.choices[0].message.content)
